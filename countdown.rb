df61d62dfdf3b61b519475d959290a182594d68f
Hannah Harrington <hannahharrington@outlook.com>
2019-09-19 13:32:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,13 @@
 #write your code here
 
-def countdown
-   SECOND(S)!"= "#{ countdown(10) }"
-  puts 
-  while countdown < 0 
-  return "HAPPY NEW YEAR!"
-  x +=1
+
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  num -= 1
+
  end
+  "HAPPY NEW YEAR!"
+end
 end
